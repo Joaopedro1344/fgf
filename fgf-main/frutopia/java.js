b4c8b023e810c0d7859277edf12bf62e898e8a5d
Samuel <142310606+SBSsamuel@users.noreply.github.com>
2024-08-06 08:57:03 -0300

    Add files via upload

---
+++ fgf-main/frutopia/java.js
@@ -0,0 +1,15 @@
+
+        function decreaseQuantity() {
+            let quantityInput = document.getElementById('quantity');
+            let currentValue = parseInt(quantityInput.value);
+            if (currentValue > 1) {
+                quantityInput.value = currentValue - 1;
+            }
+        }
+
+        function increaseQuantity() {
+            let quantityInput = document.getElementById('quantity');
+            let currentValue = parseInt(quantityInput.value);
+            quantityInput.value = currentValue + 1;
+        }
+    
